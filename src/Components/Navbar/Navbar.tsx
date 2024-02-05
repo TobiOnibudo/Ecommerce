6c5ea5164ecb61e05f0a5d43236d2fb8cad18128
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-02-04 20:09:36 -0400

    Effect on navbar completed

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,8 +2,11 @@ import "./Navbar.css"
 
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
+import { useState } from "react"
 
 function  Navbar(){
+  const [menu,setMenu] = useState("shop")
+  
     return (
       <div className="navbar">
         <div className="nav-logo">
@@ -11,10 +14,18 @@ function  Navbar(){
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop <hr /></li>
-            <li>Menu</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop
+             {menu == "shop" ? <hr/> : <></>}
+            </li>
+            <li onClick={()=>{setMenu("men")}}>Men
+             {menu == "men" ? <hr/> : <></>}
+            </li>
+            <li onClick={()=>{setMenu("women")}}>Women
+            {menu == "women" ? <hr/> : <></>}
+            </li>
+            <li onClick={()=>{setMenu("kids")}}>Kids
+            {menu == "kids" ? <hr/> : <></>}
+            </li>
         </ul>
 
         <div className="nav-login-cart">
