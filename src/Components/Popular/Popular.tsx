c06320a15c83ce0dc1300d676de0ffd0c44fb560
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-02-13 23:23:33 -0400

    Added Popular component

---
+++ src/Components/Popular/Popular.tsx
@@ -0,0 +1,27 @@
+import './Popular.css';
+import data_product from '../assets/data';
+import Item from '../Item/Item';
+
+
+
+type itemEnum = {
+  name: string;
+  image: string;
+  old_price : number;
+  new_price: number;
+}
+function Popular(){
+  return (
+    <div className="popular">
+      <h1>POPULAR IN WOMEN</h1>
+      <hr/>
+      <div className="popular-item">
+        {data_product.map((item : itemEnum, i : number)=>{
+          return <Item key={i} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> 
+        })}
+      </div>
+    </div>
+  )
+};
+
+export default Popular;
