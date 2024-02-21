5060e86f0e557f06fdf57c3f48579e37679633fa
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-02-21 00:31:12 -0400

    Added Offers to shop component

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
 function Shop(){
@@ -6,6 +7,7 @@ function Shop(){
     <div>
       <Hero />
       <Popular/>
+      <Offers/>
     </div>
   )
 };
