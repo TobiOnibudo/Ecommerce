ce14babddc3056cd8d799ead1100f7269226e312
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-01 21:33:51 -0400

    Created Shop context

---
+++ src/Context/ShopContext.tsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+import all_product from "../Components/assets/all_product";
+import Product from "../Pages/Product";
+
+
+type Product = {
+    id: number,
+    name: string,
+    category: string,
+    image: string,
+    new_price: number,
+    old_price: number,
+  }
+
+
+
+export const ShopContext = createContext<Array<Product>
+>([]);
+
+const ShopContextProvider = (props : any) => {
+    const contextValue =  all_product
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider;
+
