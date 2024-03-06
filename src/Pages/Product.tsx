06a7f109c1eab6ce6556e5cba0f4e748a71d79d3
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-05 23:15:21 -0400

    filling up content for product component

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,8 +1,17 @@
+import { useParams } from "react-router-dom";
+import { useShop,Product} from "../Context/ShopContext";
+import Breadcrumb from "../Components/Breadcrumbs/Breadcrumb";
 
 function Product(){
+  const all_product = useShop();
+  const {productId} = useParams();
+  const product = all_product.find((e : Product|undefined)=>{
+    return  e?.id === Number(productId);
+  })
+
   return (
     <div>
-      
+      <Breadcrumb product={product} />
     </div>
   )
 };
