1eeee24e6963b103388bb46433a4a96ba3f60418
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-17 21:41:37 -0300

    Adding an endpoint to test backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,34 @@
-const port = 4000;
+require('dotenv').config();
 const express = require("express");
+const port = process.env.port || 4000;
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken")
 const multer = require("multer")
 const path = require("path")
 const cors = require("cors")
+
+const userName = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+app.use(express.json());
+app.use(cors());
+
+
+//Database Connection with mongoDB
+mongoose.connect(`mongodb+srv://${userName}:${dbPassword}@cluster0.apvwpun.mongodb.net/e-commerce`)
+
+//API creation
+
+app.get('/', (req, res) => {
+    res.send(`Hello World! ${userName}`)
+})
+
+
+
+
+app.listen(port,(error) =>
+{
+    !error ? console.log(`Server Running on Port ${port}`) : 
+    console.log(`Error: ${error}`)
+})
