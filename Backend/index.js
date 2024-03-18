23a415bda49b3588d41836fe96beec3d27ba590d
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-17 21:53:31 -0300

    Upload Endpoint created and tested

--- Backend/index.js
+++ Backend/index.js
@@ -37,6 +37,14 @@ const diskStorage = multer.diskStorage(
 
 const upload =multer({storage : diskStorage})
 
+//Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res) =>{
+    res.json({
+        success : 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
 
 app.listen(port,(error) =>
 {
