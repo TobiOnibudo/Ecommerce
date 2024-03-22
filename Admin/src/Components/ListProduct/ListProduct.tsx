1eb6cf7d57aea52ac6872ccf393ebe64796e0e18
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-21 21:41:48 -0300

    Adding the delete functionality to the cross icon

--- Admin/src/Components/ListProduct/ListProduct.tsx
+++ Admin/src/Components/ListProduct/ListProduct.tsx
@@ -15,16 +15,29 @@ function ListProduct(){
     }
 
 
-  const fetchProdInfo = async () =>{
+    const fetchProdInfo = async () =>{
         const response = await fetch('http://localhost:4000/allproducts')
         const responseData = await response.json()
         console.log(responseData)
         setAllProducts(responseData)
     }
-  
+
+
+    const remove_product = async (id : number) =>{
+        await fetch('http://localhost:4000/deleteproduct',{
+            method:'POST',
+            headers:{
+                Accept: 'application/json',
+                "Content-type": "application/json"
+            },
+            body:JSON.stringify({id:id})
+        });
+
+        await fetchProdInfo();
+    }
+
     useEffect(()=>{
-        fetchProdInfo();
-    })
+        fetchProdInfo()},[])
 
     return (
     <div className='list-product'>
@@ -39,19 +52,19 @@ function ListProduct(){
         </div>
         <div className="listproduct-allproducts">
             <hr />
-            {allproducts.map((product : Product ,index) =>{
+            {allproducts.map((product : Product ,index : React.Key) =>{
                 return (
-                    <>
-                        <div key={index} className="listproduct-format-main listproduct-format">
+                    <div key={index}>
+                        <div className="listproduct-format-main listproduct-format">
                             <img src={product.image} alt="" className="listproduct-product-icon" />  
                             <p>{product.name}</p>
                             <p>${product.old_price}</p>
                             <p>${product.new_price}</p>
                             <p>{product.category}</p>
-                            <img className="listproduct-remove-icon" src={cross_icon} alt="" />
+                            <img onClick={()=>{remove_product(product.id)}} className="listproduct-remove-icon" src={cross_icon} alt="" />
                         </div>
-                        <hr/>
-                    </>
+                        <hr />
+                    </div>
                 )
             })}
         </div>
