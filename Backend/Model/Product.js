d194dc6048555a8b63cc52a6695b0720f4618964
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-24 18:11:15 -0300

    Created User Model and Schema

--- Backend/Model/Product.js
+++ Backend/Model/Product.js
@@ -1,5 +1,7 @@
-import { productSchema } from "../Schema/Product.js"
+import { productSchema , UserSchema} from "../Schema/Product.js"
 import mongoose from "mongoose"
-const Product = mongoose.model("Product",productSchema)
+export const Product = mongoose.model("Product",productSchema)
+export const User = mongoose.model("User",UserSchema)
+
 
 export default Product
