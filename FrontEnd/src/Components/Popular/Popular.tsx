2c1388fd29f4fa1549cdacb2657b9069cbbcc33b
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-03-27 19:37:31 -0300

    Using popular in women endpoint

--- FrontEnd/src/Components/Popular/Popular.tsx
+++ FrontEnd/src/Components/Popular/Popular.tsx
@@ -1,6 +1,7 @@
 import './Popular.css';
 import data_product from '../assets/data';
 import Item from '../Item/Item';
+import { useEffect,useState } from 'react';
 
 
 
@@ -12,12 +13,24 @@ type itemEnum = {
   new_price: number;
 }
 function Popular(){
+  const [popularProducts,setPopularProducts] = useState([])
+
+  useEffect(()=>{
+    fetchPopular()
+  },[])
+
+  const fetchPopular = async () =>{
+    const response = await fetch("http://localhost:4000/popular/women")
+    const responseData = await response.json()
+    console.log(responseData)
+    setPopularProducts(responseData)
+  }
   return (
     <div className="popular">
       <h1>POPULAR IN WOMEN</h1>
       <hr/>
       <div className="popular-item">
-        {data_product.map((item : itemEnum, i : number)=>{
+        {popularProducts?.map((item : itemEnum, i : number)=>{
           return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> 
         })}
       </div>
